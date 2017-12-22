7c828cbbf69803996bf9c15e5878664c047c9a4f
newbyuk123 <alastair.g.newby@gmail.com>
2017-12-22 10:04:33 +0000

    first commit

---
+++ garage.js
@@ -0,0 +1,6 @@
+const addCar = ()=>{
+    const make = document.getElementById('make').value;
+    const model = document.getElementById('model').value;
+    const year = document.getElementById('year').value;
+    const fault = document.getElementById('faults').value;
+}
