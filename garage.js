cf4546ba6d467b1bd0651ce269f1c7c6d5ae59e6
dasp24 <daniel.parkes901@gmail.com>
2017-12-22 10:17:38 +0000

    commit

--- garage.js
+++
@@ -1,6 +0,0 @@
-const addCar = ()=>{
-    const make = document.getElementById('make').value;
-    const model = document.getElementById('model').value;
-    const year = document.getElementById('year').value;
-    const fault = document.getElementById('faults').value;
-}
